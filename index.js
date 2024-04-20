8c10f3fbcf95e041f1d8963ddfb2c62206307d43
Matsafu <123011242+Matsafu@users.noreply.github.com>
2024-04-20 23:45:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function search(event) {
   event.preventDefault();
   let citySearch = document.querySelector("#city-search");
-  let city = citySearch.value;
-  let apiKey = `cat494bb9c91fa07d373647010ob64af`;
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(displayWeather);
 }
