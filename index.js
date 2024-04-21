cc284aa1bcdb5d235b02ee8fb2a0af82af42029f
Matsafu <123011242+Matsafu@users.noreply.github.com>
2024-04-21 20:27:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+function currentWeather(response) {
+  let temperature = document.querySelector("#weather-temp");
+  let temp = response.data.temperature.current;
+  temperature.innerHTML = Math.round(temp);
+}
+function weatherSearch(city) {
+  let apiKey = "cat494bb9c91fa07d373647010ob64af";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeather);
+}
 function search(event) {
   event.preventDefault();
-  let citySearch = document.querySelector("#city-search");
+  let searchInput = document.querySelector("#form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  weatherSearch(searchInput.value);
 }
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit", search);
